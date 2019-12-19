642a392fa434ff9f684b7277c70678ca6f6fa02c
guilhembn <gbuisan@laas.fr>
2019-12-19 22:11:54 +0100

    Fixes changes in calibration file case

--- src/cameraParams.cpp
+++ src/cameraParams.cpp
@@ -12,7 +12,13 @@ CameraParams::CameraParams(const std::string &filename) {
   fs["image_Width"] >> size.width;
   fs["image_Height"] >> size.height;
   fs["Camera_Matrix"] >> matrix;
+  if (matrix.cols == 0){
+    fs["camera_matrix"] >> matrix;
+  }
   fs["Distortion_Coefficients"] >> distortionCoeffs;
+  if (distortionCoeffs.cols == 0){
+    fs["distortion_coefficients"] >> distortionCoeffs;
+  }
   fs.release();
 }
 
